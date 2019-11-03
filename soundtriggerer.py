1d5105978df7d46d9ceb6c4e27f1826432a2cb19
bruol <lorin@urbantat.eu>
2019-11-03 18:55:37 +0100

    dictionary

--- soundtriggerer.py
+++ soundtriggerer.py
@@ -5,44 +5,70 @@ import time
 pygame.init()
 pygame.mixer.init(allowedchanges=0)
 
+
+
+
 #load sfx
 phone = pygame.mixer.Sound('files/audio.ogg')
 
 #play sfx
 #phone.play()
 
+
+
+#load background image
 image = pygame.image.load("files/keyboard.png")
 
+
+#define window width to background image dimensions
 width=image.get_rect()[2]
 height=image.get_rect()[3]
 
+# define offset of first key from border
 x = 5
 y = 5
 
+#define gaps between keys
 x_gap = 3
 y_gap = 5
 
+#define size of square keys
 key_dim = 49
 
-#define screen
-screen = pygame.display.set_mode((width, height))
+#define window
+window = pygame.display.set_mode((width, height))
+
+#define that we are not done jet
 done = False
 
+#Todo:rename
 is_blue = True
 
+#define how fast loop will be running 
 clock = pygame.time.Clock()
+
+
+keys = {'1':(1,1),'2':(2,1),'3':(3,1),'4':(4,1),'5':(5,1),'6':(6,1),'7':(7,1),'8':(8,1),'9':(9,1),'0':(10,1),'ß':(11,1),'´':(12,1),
+        'q':(1,2),'w':(2,2),'e':(3,2),'r':(4,2),'t':(5,2),'z':(6,2),'u':(7,2),'i':(8,2),'o':(9,2),'p':(10,2),'ü':(11,2),'+':(12,2),
+        '^':(1,3),'a':(2,3),'s':(3,3),'d':(4,3),'f':(5,3),'g':(6,3),'h':(7,3),'j':(8,3),'k':(9,3),'l':(10,3),'ö':(11,3),'ä':(12,3),
+        '<':(1,4),'y':(2,4),'x':(3,4),'c':(4,4),'v':(5,4),'b':(6,4),'n':(7,4),'m':(8,4),',':(9,4),'.':(10,4),'-':(11,4),'#':(12,4),}
 
 
 #main loop
 while not done:
-        screen.fill((0,0,0))
-        #SHOW BACKGROUND IMAGE 
-        screen.blit(image, (0, 0))
+
+        #make window black
+        window.fill((0,0,0))
+
+        #show background image
+        window.blit(image, (0, 0))
 
 
         #check for event
         for event in pygame.event.get():
+                #check for keypresses
                 if event.type == pygame.KEYDOWN:
+                        #move squares
                         if event.key == pygame.K_SPACE:
                                 is_blue = not is_blue
                         if event.key == pygame.K_UP:
@@ -62,13 +88,13 @@ while not done:
 
         for j in range(4):
                 for i in range(12):
-                        #draw rectangle on screen                        
+                        #draw rectangle on window                        
                         s = pygame.Surface((49,49))
                         s.set_alpha(50)
                         s.fill(color)
-                        screen.blit(s,(x+i*(key_dim+x_gap),y+j*(key_dim+y_gap)))
+                        window.blit(s,(x+i*(key_dim+x_gap),y+j*(key_dim+y_gap)))
 
-                        #pygame.draw.rect(screen, color, pygame.Rect(x+i*(key_dim+x_gap), y+j*(key_dim+y_gap), 49, 49))
+                        #pygame.draw.rect(window, color, pygame.Rect(x+i*(key_dim+x_gap), y+j*(key_dim+y_gap), 49, 49))
 
 
         #update view
