961cd0d707e83df1b7b7150ad14bc229d3dd2451
bruol <lorin@urbantat.eu>
2019-11-04 19:27:08 +0100

    fullscreen working

--- soundtriggerer.py
+++ soundtriggerer.py
@@ -17,16 +17,23 @@ image = pygame.image.load("files/keyboard.png")
 #define window width to background image dimensions
 width=image.get_rect()[2]
 height=image.get_rect()[3]
-# define offset of first key from border
-x_margin = 5
-y_margin = 5
-#define gaps between keys
-x_gap = 3
-y_gap = 5
-#define size of square keys
-key_dim = 49
+
 #define window
-window = pygame.display.set_mode((width,height))
+window = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+
+w,h = pygame.display.get_surface().get_size()
+
+param = w/width
+
+# define offset of first key from border
+x_margin = param * 5
+y_margin = param * 5
+#define gaps between keys
+x_gap = param * 3
+y_gap = param * 5
+#define size of square keys
+key_dim = param * 49
+
 #define that we are not done jet
 done = False
 
@@ -39,7 +46,7 @@ color = pygame.Color(0, 128, 255)
 clock = pygame.time.Clock()
 
 
-myfont = pygame.font.SysFont("Noto Sans",9,True)
+myfont = pygame.font.SysFont("Noto Sans",int(param*9),True)
 
 
 keys = {'1':(0,0),'2':(1,0),'3':(2,0),'4':(3,0),'5':(4,0),'6':(5,0),'7':(6,0),'8':(7,0),'9':(8,0),'0':(9,0),'ß':(10,0),'´':(11,0),
@@ -53,7 +60,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.color = (color)                     
-        self.image = pygame.Surface((49,49))
+        self.image = pygame.Surface((key_dim,key_dim))
         self.image.set_alpha(0)
         self.image.fill(self.color)
         self.rect = self.image.get_rect()  # Get rect of some size as 'image'.
@@ -69,7 +76,6 @@ class Player(pygame.sprite.Sprite):
                 self.image.set_alpha(0)
 
 
-
 player = Player()
 
 dt_prev = 0
@@ -78,24 +84,22 @@ sounds = {'q':'phone','w':'door','e':'break','r':'durch_fr','t':'durch_sa','z':'
 #main loop
 while not done:
         dt = clock.tick(fps) / 1000
-        
 
-        dt_prev += dt
+
         
         #make window black
         window.fill((0,0,0))
 
         #show background image
 
-        window.blit(image, (0, 0))
+        window.blit(pygame.transform.scale(image,(int(param*width),int(param*height))), (0, 0))
         
+        print(param)
         #write text
-
-
         for element in sounds:
                 label = myfont.render(sounds[element],1,(255,255,255))
 
-                window.blit(label, (5 + x_margin + keys[element][0]*(key_dim + x_gap),30+y_margin + keys[element][1]*(key_dim + y_gap)))
+                window.blit(label, (  5 + x_margin + keys[element][0]*(key_dim + x_gap)  , param*30+y_margin + keys[element][1]*(key_dim + y_gap)  ))
 
 
 
